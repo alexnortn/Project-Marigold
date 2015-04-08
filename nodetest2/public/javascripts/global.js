a02d057a23230f3c1db03267a12e2246289f7a99
alexnortn <alexnortn@gmail.com>
2015-04-08 11:19:24 -0400

    added vimeo

--- nodetest2/public/javascripts/global.js
+++ nodetest2/public/javascripts/global.js
@@ -26,6 +26,8 @@ $(document).ready(function() {
 
         !open ? $('.project-contents').fadeOut('slow') : $('.project-contents').fadeIn('slow') 
 
+        newWidth = $fluidEl.width();
+
         $('#logo p').fadeToggle(1000);
         $('#pagination').fadeToggle(1000);
         $('#arrow').fadeToggle(1000);
@@ -55,6 +57,48 @@ $(document).ready(function() {
 
     $('#logo').click(function(){
         closeProject();
+    });
+
+    $(function() {
+
+        // Find all Vimeo videos
+        var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com']"),
+
+        // The element that is fluid width
+        $fluidEl = $("#video-frame");
+        var newWidth = $fluidEl.width();
+
+        // Figure out and save aspect ratio for each video
+        $allVideos.each(function() {
+
+          $(this)
+            .data('aspectRatio', this.height / this.width)
+
+            // and remove the hard coded width/height
+            .removeAttr('height')
+            .removeAttr('width');
+
+        });
+
+        // When the window is resized
+        $(window).resize(function() {
+
+                newWidth = $fluidEl.width();
+                console.log(newWidth);
+
+            // Resize all videos according to their own aspect ratio
+            $allVideos.each(function() {
+
+                var $el = $(this);
+                $el
+                  .width(newWidth)
+                  .height(newWidth * $el.data('aspectRatio'));
+
+            });
+
+        // Kick off one resize to fix all videos on page load
+        }).resize();
+
     });
 
 
