dd61ba712b48ac3fe45460c7e630156509f88500
Alex Norton <alexnortn@gmail.com>
2016-04-21 12:05:20 -0400

    resolving directory structure

---
+++ portfolio/routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router  = express.Router();
+
+
+// Declare variables
+var fs = require('fs'),
+    contents,
+    bios_text,
+    welcome_text,
+    projects;
+
+// Read the file and send to the callback
+fs.readFile('./public/javascripts/content.json', handleFile)
+
+// Write the callback function
+function handleFile(err, data) {
+    if (err) throw err
+    contents = JSON.parse(data)
+    // You can now play with your datas
+    console.log(contents);
+    projects = contents.projects;
+    bios_text = contents.bios_text;
+    welcome_text = contents.welcome_text;
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+  	title: 'Alex Norton',
+  	projects: projects,
+    welcome_text: welcome_text,
+    bios_text: bios_text
+  });
+});
+
+module.exports = router;
