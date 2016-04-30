55e03e93293031c3a7a32fc3dbf6e9b3fdcbd949
Alex Norton <alexnortn@gmail.com>
2016-04-30 16:17:57 -0400

    ready to add perlin attractors

--- portfolio/clientjs/p5/sketch.js
+++ portfolio/clientjs/p5/sketch.js
@@ -59,28 +59,17 @@ let glyph = function (p) {
 		timeOut,
 		timeOut1,
 		alphaOpa1,
-		buttonFade,
-		buttonFade1,
 		lineOp,
 		glyphOp,
 		tempSelection,
 		aCenterOffset,
-		liveText,
-		describeText,
-		describeText1,
-		hover,
 		clockBool,
 		bigGlyph,
-		interestsArr = [],
 		aVerts = [],
 		aCounterVerts = [],
 		nVerts = [];
 
 	let nudgeAttractor; 
-	let dashButton;
-
-	let descriptiveText  = "This interactive visualization of interest was developed using the P5js framework along with the Toxiclibs physics library."; 
-	let descriptiveText1 = "Vertices used to construct the character are influenced by an underlying physics system and mapped to extend towards difference areas of my interest.";
 
 	p.preload = function() {
 		// vertices = p.loadJSON("clientjs/p5/data/verts.json");
@@ -116,12 +105,9 @@ let glyph = function (p) {
 		alphaOpa = 0;
 		alphaOpa1 = 0;
 		glyphOp = 0;
-		buttonFade = 0;
-		buttonFade1 = 0;
 		lineOp = 0;
 		timeOut = 500;
 		timeOut1 = 400;
-		hover = false;
 
 		// Address N Scaling
 		nScaleFactor = 0.3;
@@ -166,27 +152,6 @@ let glyph = function (p) {
 			p: p,
 		});
 
-		// Make the button
-		dashButton = new MakeButton ({
-			x: 0,
-			y: 0,
-			buttonWidth: 50,
-			p: p,
-		});
-
-		// Create text node
-		liveText = p.createP().id('interests');
-		liveText.parent('interactive');
-		liveText.style("opacity", "0");
-		describeText = p.createP(descriptiveText).class('describe');
-		describeText.parent('interactive');
-		describeText.style("opacity", "0");
-		describeText1 = p.createP(descriptiveText1).class('describe');
-		describeText1.parent('interactive');
-		describeText1.style("opacity", "0");
-
-		interestsArr.push('Artificial Intelligence','Neuroscience','Game Design','Graphic Design','Architecture','Urban Design','Physics','Computation');
-
 	}
 
 	p.draw = function() {
@@ -205,22 +170,8 @@ let glyph = function (p) {
 		drawBasicA();
 		drawBasicN();
 
-		// Set timeout for loading the clockViz
-		if (timeOut > 0) timeOut--;
-		if ((timeOut == 0) && (clockBool)) {
-			// clockViz(); 
-		} 
-
-		if (timeOut1 > 0) timeOut1--;
-		if (timeOut1 == 0) {
-			fadeInButton();
-		} 
-
 		// Display the Physiscs Particles;
 		displayPhys();
-
-		// Button Interactivity
-		hoverButton1();
 
 	}
 
@@ -262,9 +213,9 @@ let glyph = function (p) {
 	function drawBasicA(){
 		let fadeSpeed = 2;
 		if (glyphOp < 255) glyphOp += fadeSpeed;
-		// p.noStroke();
-		p.stroke(1);
-		// p.fill(35,35,35,glyphOp);
+		// p.stroke(1);
+		p.noStroke();
+		p.fill(35,35,35,glyphOp);
 		p.beginShape();
 		p.vertex(aSpringVert[0].x, aSpringVert[0].y);
 		p.vertex(aSpringVert[1].x, aSpringVert[1].y);
@@ -302,9 +253,9 @@ let glyph = function (p) {
 	function drawBasicN(){
 		let fadeSpeed = 2;
 		if (glyphOp < 255) glyphOp += fadeSpeed;
-		p.stroke(1);
-		// p.noStroke();
-		// p.fill(35,35,35,glyphOp);
+		// p.stroke(1);
+		p.noStroke();
+		p.fill(35,35,35,glyphOp);
 		p.beginShape();
 			p.vertex(nSpringVert[0].x, nSpringVert[0].y);
 			p.vertex(nSpringVert[1].x, nSpringVert[1].y);
@@ -331,83 +282,6 @@ let glyph = function (p) {
 				p.strokeWeight(1);
 				p.stroke(0);
 				p.line(springVector.x, springVector.y, rayVector.x, rayVector.y);
-	}
-
-	function clockDisp(alphaOpa) {
-		let interests = 8;
-		let tempLoc = p.createVector();
-		let theta = p.TAU / interests;
-		let radLen = ( w < h ? w : h ) / 2.75;
-		let fadeSpeed1 = 10;
-
-		for (let i = 0; i < interests; i++) {
-			let selection;
-			let x = (radLen * p.sin(theta * i)) + center.x;
-			let y = (radLen * p.cos(theta * i)) + center.y - 50; // 50 = relative y offset
-
-			// Display the clock p.ellipses
-			p.fill(0,0,0, alphaOpa);
-			p.noStroke();
-			p.ellipse(x, y, 8, 8);
-
-			// Calc distance to local point in clock array
-			tempLoc.set(x,y);
-			let dist1 = mousePos.dist(tempLoc);
-			if (dist1 < 75) {
-				// FadeIn content
-				cursor(CROSS);
-				if (alphaOpa1 < 255) alphaOpa1 += fadeSpeed1;
-				tempSelection = i;
-				nudgeAttractor.hover();
-
-			} else {
-				// FadeOut content
-				if (i == tempSelection) {
-					while (alphaOpa1 > 30) {
-						p.cursor(ARROW);
-						alphaOpa1 -= fadeSpeed1 * 3;  
-					} if ((alphaOpa1 < 30) && (alphaOpa1 > 0)) alphaOpa1 -= fadeSpeed1 / 10;  
-
-					nudgeAttractor.away();
-				}
-			}
-			if (i == tempSelection) {
-				// Display the proper interest type if hovered
-				if (alphaOpa1 > 0) {
-					interestDisp(i, x, y, alphaOpa1);
-					dashedCircle(x, y, alphaOpa1);
-					displayPhys1(x, y, alphaOpa1);
-				}
-			}
-		}
-
-	}
-
-	function clockViz() {
-		let fadeSpeed = 2;
-			if ((w > 1000) && (h > 850)) {
-				if (alphaOpa < 255) alphaOpa += fadeSpeed;
-			} else {
-				if (alphaOpa > 0) alphaOpa -= fadeSpeed;
-				// Also control button fading as well
-				fadeOutButton();
-			}
-		if (alphaOpa > 0) clockDisp(alphaOpa);
-	}
-
-	function dashedCircle(x,y,opacity) {
-		// Dashed line around point using Polar Coordinates
-		let circleRad = 12;
-		let pointAmount = 25;
-		let theta1 = p.TAU / pointAmount;
-		for (let j = 0; j < pointAmount; j++) {
-			let x1 = (circleRad * p.sin(theta1 * j)) + x;
-			let y1 = (circleRad * p.cos(theta1 * j)) + y;
-			p.push();
-				p.fill(0,0,0,opacity);
-				p.ellipse(x1, y1,1,1);
-			p.pop();
-		}
 	}
 
 	// Setup the dynamic arrays --> center them on the page
@@ -690,14 +564,15 @@ let glyph = function (p) {
 
 	// A little too much interaction, if you ask me! Might be useful later
 
-	function mouseClicked() {
+	p.mouseClicked = function() {
 		physics.removeBehavior(gravity);
 		// Use perlin noise to achieve a similar* gravity vector
 		xOff += 0.5;
+		console.log('mouse clicked')
 		// Horizontal + Vertical leaping may be too much...
-		// gravityStrength.x = noise(xOff) * 0.75;
+		gravityStrength.x = p.noise(xOff) * 0.75;
 		gravityStrength.y *= -1;
-		gravity = toxi.physics2d.behaviors.GravityBehavior(gravityStrength);
+		gravity = new toxi.physics2d.behaviors.GravityBehavior(gravityStrength); // Re-initialize gravity
 		physics.addBehavior(gravity);
 		// prevent default
 		return false;
@@ -709,106 +584,6 @@ let glyph = function (p) {
 			p.fill(255, 100);
 			p.rect(0,0,width,height);
 		p.pop();
-	}
-
-	// Display typography on hover
-	function interestDisp(i, x, y, opacity) {
-
-		i < 5 ? liveText.style("text-align", "left") : liveText.style("text-align", "right");
-		// X offset from point
-		i < 5 ? (x += 30) : (x -= 125);
-		if (i > 4) y -= 3;
-		if (i == 6) x += 45;
-		let textOpacity = p.map(opacity, 0, 255, 0, 1);
-		liveText.style("opacity", textOpacity);
-		liveText.html(interestsArr[i]);
-		liveText.position(x,y-7);
-
-	}
-
-	// Hover button interactivity
-	function hoverButton() {
-		let fadeSpeed = 20;
-		let mousey = p.createVector(dashButton.loc().x, dashButton.loc().y);
-		let overButton = mousey.dist(mousePos);
-		let lineOffset = 60;
-		let lineLoc;
-		let opacity;
-
-		lineLoc = p.map(buttonFade, 0, 255, 0, lineOffset);
-		opacity = p.norm(buttonFade, 0, 255);
-
-		// Position the descriptive text along with the button
-		describeText.position(mousey.x, mousey.y -95);
-		describeText.style("opacity", opacity);
-		describeText1.position(mousey.x, mousey.y -35);
-		describeText1.style("opacity", opacity);
-
-	 p.push();
-			p.stroke(0,0,0,lineOp);
-			p.fill(0,0,0,lineOp);
-			p.strokeWeight(1);
-			if (timeOut == 0) {
-				line (mousey.x, mousey.y, mousey.x, mousey.y - lineLoc);
-				line (mousey.x, mousey.y, mousey.x, mousey.y + lineLoc);
-			}
-		p.pop();
-
-		if (overButton < 50) { 
-			lineOp = 255;
-			p.cursor(HAND);
-			dashButton.hover(width,height);
-			 if (buttonFade < 255) buttonFade += fadeSpeed;
-			 if (alphaOpa > 0) alphaOpa -= fadeSpeed;
-		} else {
-			dashButton.display(width,height);
-			if (buttonFade > 60) {
-				buttonFade -= fadeSpeed * 4;
-				p.cursor(ARROW);
-			} else  if (buttonFade > 0) buttonFade -= fadeSpeed / 2;
-		}
-
-	}
-
-	// Hover button interactivity
-	function hoverButton1() {
-		let fadeSpeed = 20;
-		let opacity;
-
-		opacity = p.norm(buttonFade, 0, 255);
-
-		let element = $('#cross-circle');
-
-		element.hover(function() {
-			hover = true;
-		});
-
-		element.mouseout(function() {
-			hover = false;
-		});
-
-		if (hover) { 
-			lineOp = 255;
-			 if (buttonFade < 255) buttonFade += fadeSpeed;
-			 if (alphaOpa > 0) alphaOpa -= fadeSpeed;
-		} else {
-			if (buttonFade > 60) {
-				buttonFade -= fadeSpeed * 4;
-			} else  if (buttonFade > 0) buttonFade -= fadeSpeed / 2;
-		}
-
-	}
-
-	function fadeInButton() {
-		let fadeSpeed = 5;
-		if (buttonFade1 < 255) buttonFade1 += fadeSpeed;
-		dashButton.opa(buttonFade1);
-	}
-
-	function fadeOutButton() {
-		let fadeSpeed = 5;
-		if (buttonFade1 > 0) buttonFade1 -= fadeSpeed;
-		dashButton.opa(buttonFade1);
 	}
 
 	// Deal with resize events
