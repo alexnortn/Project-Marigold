8df7cd0c5c80b04970fa484e6c84537657ee0777
Alex Norton <alexnortn@gmail.com>
2016-08-14 19:40:05 -0400

    added favicon

--- app.js
+++ app.js
@@ -31,8 +31,8 @@ app.set('port', PORT);
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
