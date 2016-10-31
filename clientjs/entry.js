6b32617a81cc53e6ecb0fc95515c421060c34247
Alex Norton <alexnortn@gmail.com>
2016-10-30 21:20:21 -0400

    more work into curation of projects

--- clientjs/entry.js
+++ clientjs/entry.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
     	? true
     	: false;
 
-	activateGlyph(_mobile, phone, tablet); // Save battery while testing
+	// activateGlyph(_mobile, phone, tablet); // Save battery while testing /
 
 });
 
