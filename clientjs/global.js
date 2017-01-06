e13ed33d79016968c14f63ecfa4cc58521ac896c
Alex Norton <alexnortn@gmail.com>
2017-01-06 12:12:28 -0500

    define section coming along

--- clientjs/global.js
+++ clientjs/global.js
@@ -230,6 +230,12 @@ $(document).ready(function() {
         $(this).parent().velocity("fadeOut", { duration: 250 });
     });
 
+    // Toggle "More" content for Museum Project Requirements
+    $('#ghost-project-requirements').click(function(evt) {
+        $('#ghost-project-requirements-affect').removeClass('endeavor-reveal');
+        $(this).parent().velocity("fadeOut", { duration: 250 });
+    });
+
     // Create SVG Circle
     function createCircle(_cx, _cy) {
         let svgNS = "http://www.w3.org/2000/svg";  
